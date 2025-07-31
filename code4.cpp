0268742d9672c6463f175f993aff04cc0697e25e
divvyanshi06 <divvyanshi06@gmail.com>
2025-08-01 01:13:04 +0530

    Right Angled Triangle

---
+++ code4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number of rows: "<<endl;
+    cin>>n;
+    
+    for (int i=1 ; i<=n ; i++){       // It decides how many rows will get printed
+        for (int j=1 ; j<=i ; j++){   //
+            cout<<"* ";
+        }
+        cout<< endl;
+    }
+
+    return 0;
+}
