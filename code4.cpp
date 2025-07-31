403838a46b9b8d0c0552791e393d715b997c1386
divvyanshi06 <divvyanshi06@gmail.com>
2025-08-01 01:33:30 +0530

    Update code4.cpp

--- code4.cpp
+++ code4.cpp
@@ -16,3 +16,23 @@ int main()
 
     return 0;
 }
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number of rows: "<<endl;
+    cin>>n;
+    
+    for (int i=n ; i>=1 ; i--){       // It decides how many rows will get printed
+        for (int j=1 ; j<=i ; j++){   //
+            cout<<"* ";
+        }
+        cout<< endl;
+    }
+
+    return 0;
+}
