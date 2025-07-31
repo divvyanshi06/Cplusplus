35b8944c96d16c55bb96c7c17adb788d1c31ab67
divvyanshi06 <divvyanshi06@gmail.com>
2025-08-01 00:52:24 +0530

    Age and Name category checker program

---
+++ code1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout<<"Hello Buddy"<<endl;
+    cout<<"Enter your name :"<<endl;
+    cin>>name;
+    cout<<"Enter your age : "<<endl;
+    cin>>age;
+    cout<<"Your name is "<<name<<" and your age is "<<age<<endl;
+
+    return 0;
+}
